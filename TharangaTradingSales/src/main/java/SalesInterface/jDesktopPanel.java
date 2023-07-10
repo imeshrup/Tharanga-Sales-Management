d2ecc71be0106a0b43e712f0df581af17eda1329
imeshrup <117440415+imeshrup@users.noreply.github.com>
2023-07-10 14:19:42 +0530

    Add files via upload

---
+++ TharangaTradingSales/src/main/java/SalesInterface/jDesktopPanel.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SalesInterface;
+
+/**
+ *
+ * @author IMESH
+ */
+class jDesktopPanel {
+
+    static void removeAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static Object add(SalesMenu m2) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
